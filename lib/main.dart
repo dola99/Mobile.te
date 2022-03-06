e467e91a82158c1e387cf3ca70489f1247a20148
Mohamed Adel <haaam25@gmail.com>
2022-03-06 12:39:35 +0200

    remove . to fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
